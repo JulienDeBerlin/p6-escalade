c18adbf11c4e8f46054e0af1383abf03763b18f6
JulienDeBerlin <julien_berthoud@yahoo.fr>
2019-01-30 15:15:19 +0100

    correction POMs

--- consumer-impl-jdbc/src/main/java/com/berthoud/ocp6/consumer/impl/jdbc/MemberDaoImpl.java
+++ consumer-impl-jdbc/src/main/java/com/berthoud/ocp6/consumer/impl/jdbc/MemberDaoImpl.java
@@ -1,6 +1,6 @@
 package com.berthoud.ocp6.consumer.impl.jdbc;
 
-import com.berthoud.ocp6.consumer.dao.contract.MemberDao;
+import com.berthoud.ocp6.consumer.contract.dao.MemberDao;
 import com.berthoud.ocp6.model.bean.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
